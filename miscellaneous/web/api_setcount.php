b371de6d3a46377dd35d20d1019c74ccc9fb5738
Socialz <socialz.fin@gmail.com>
2014-03-14 16:49:10 +0200

    api-pc: mysqli
    Not sure if this works, I prefer PDO over this stuff.

--- miscellaneous/web/api_setcount.php
+++ miscellaneous/web/api_setcount.php
@@ -3,6 +3,6 @@
 	require_once("mysql_init.php");
 	$input = mta::getInput();
 	$playerCount = $input[0];
-	mysql_query("UPDATE `your_stat_table` SET `value` = '" . $playerCount . "' WHERE `key` = '1'");
+	mysqli_query($handler, "UPDATE `your_stat_table` SET `value` = '" . $playerCount . "' WHERE `key` = '1'");
 	mta::doReturn(true);
 ?>
