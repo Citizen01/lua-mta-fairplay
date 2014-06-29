734b0f2bac992e4285fddeee49b7a3275d7945e8
Socialz <socialz.fin@gmail.com>
2014-06-29 13:37:02 +0300

    initializer: fixed blackscreen issue
    ... also took out the download thing, which isn't even meant to be
    working.

--- resources/roleplay-initializer/c_initializer.lua
+++ resources/roleplay-initializer/c_initializer.lua
@@ -17,56 +17,8 @@
 	(c) Copyright 2014 FairPlay Gaming. All rights reserved.
 ]]
 
-local _dis = false
-local _dir1 = false
-local _x = 700
-local _y = 130
-
-local function showDownloadDX()
-	if (_dir1 == false) then
-		_x = _x-1
-	else
-		_x = _x+1
+addEventHandler("onClientResourceStart", resourceRoot,
+	function()
+		triggerEvent(":_initRealStartup_:", localPlayer)
 	end
-	
-	if (_x >= 700) then
-		_dir1 = false
-	elseif (_x <= 670) then
-		_dir1 = true
-	end
-	
-	if (_dir2 == false) then
-		_y = _y-1
-	else
-		_y = _y+1
-	end
-	
-	if (_y >= 130) then
-		_dir2 = false
-	elseif (_y <= 80) then
-		_dir2 = true
-	end
-	
-	dxDrawRectangle(0, 0, sx, sy, tocolor(0, 0, 0, 0.95*255), true)
-	dxDrawText("Initializing the system...", sx-_x, sy-_y, sx, sy, tocolor(245, 245, 245, 0.9*255), 1.5, "bankgothic", "left", "top", true, false, true, false, false)
-	
-	if (not isTransferBoxActive()) then
-		initStartup()
-	end
-end
-
-function initStartup(resource)
-	if (isTransferBoxActive()) then
-		_dis = true
-		addEventHandler("onClientRender", root, showDownloadDX)
-	else
-		if (_dis == true) then
-			removeEventHandler("onClientRender", root, showDownloadDX)
-		end
-		
-		if (getResourceName(resource) == "roleplay-accounts") then
-			triggerEvent(":_initRealStartup_:", localPlayer)
-		end
-	end
-end
-addEventHandler("onClientResourceStart", root, initStartup)
+)
