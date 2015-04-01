Citizen01/lua-mta-fairplay

commit eec6a658ad12a2aa9c83003b6faaa44deacf7d0e

sql-dump: fixed phone contacts saving incorrectly

sql-dump/database.sql
@@ -98,8 +98,8 @@ INSERT INTO `config` (`id`, `key`, `value`) VALUES
 CREATE TABLE IF NOT EXISTS `contacts` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `number` int(10) unsigned NOT NULL,
-  `ownerNumber` int(10) unsigned NOT NULL,
+  `number` varchar(10) unsigned NOT NULL,
+  `ownerNumber` varchar(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 

commit 37d6ebef707e26532233563236baac03e49c8f67

dump: resolved #15 (problem introduced in eec6a65)

sql-dump/database.sql
@@ -98,8 +98,8 @@ INSERT INTO `config` (`id`, `key`, `value`) VALUES
 CREATE TABLE IF NOT EXISTS `contacts` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `number` varchar(10) unsigned NOT NULL,
-  `ownerNumber` varchar(10) unsigned NOT NULL,
+  `number` varchar(10) NOT NULL,
+  `ownerNumber` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 